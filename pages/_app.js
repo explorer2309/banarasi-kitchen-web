385bbc78eb26092ba1d251bbb6f138f4836a2dec
PB <puneetsbhasin@gmail.com>
2021-01-14 22:08:46 +0000

    Footer styling

--- pages/_app.js
+++ pages/_app.js
@@ -14,51 +14,49 @@ function MyApp({ Component, pageProps }) {
 
         <footer className={global.footer}>
           <div className="">
-            <h3 className="">Connect with Us</h3>
-            <div className="">
-              <ul className="">
-                <li>
-                  <a
-                    href="https://www.instagram.com/banarasi-kitchen/"
-                    target="_blank"
-                    rel="noopener">
-                    <i className="fab fa-instagram"></i> Like us on Instagram
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://www.facebook.com/banarasi-kitchen/"
-                    target="_blank"
-                    rel="noopener">
-                    <i className="fab fa-facebook-f"></i> Follow us on Facebook
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://twitter.com/banarasi-kitchen/"
-                    target="_blank"
-                    rel="noopener">
-                    <i className="fab fa-twitter"></i> Follow us on Twitter
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://www.pinterest.co.uk/banarasi-kitchen/"
-                    target="_blank"
-                    rel="noopener">
-                    <i className="fab fa-pinterest-p"></i> Pin us on Pinterest
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://banarasi-kitchen.tumblr.com/"
-                    target="_blank"
-                    rel="noopener">
-                    <i className="fab fa-tumblr"></i> Follow us on Tumblr
-                  </a>
-                </li>
-              </ul>
-            </div>
+            <h3 className={global.footerHeader}>Connect with Us</h3>
+            <ul className={global.footerItems}>
+              <li>
+                <a
+                  href="https://www.instagram.com/banarasi-kitchen/"
+                  target="_blank"
+                  rel="noopener">
+                  <i className="fab fa-instagram"></i> Like us on Instagram
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.facebook.com/banarasi-kitchen/"
+                  target="_blank"
+                  rel="noopener">
+                  <i className="fab fa-facebook-f"></i> Follow us on Facebook
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://twitter.com/banarasi-kitchen/"
+                  target="_blank"
+                  rel="noopener">
+                  <i className="fab fa-twitter"></i> Follow us on Twitter
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.pinterest.co.uk/banarasi-kitchen/"
+                  target="_blank"
+                  rel="noopener">
+                  <i className="fab fa-pinterest-p"></i> Pin us on Pinterest
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://banarasi-kitchen.tumblr.com/"
+                  target="_blank"
+                  rel="noopener">
+                  <i className="fab fa-tumblr"></i> Follow us on Tumblr
+                </a>
+              </li>
+            </ul>
           </div>
         </footer>
       </main>
